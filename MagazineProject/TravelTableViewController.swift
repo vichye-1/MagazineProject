a495f19fd0ee1766cbde2930f815664cf9e528a6
knockknock <yangban11111@gmail.com>
2024-05-25 17:40:03 +0900

    Add: subtitle label 추가 / kingfisher 라이브러리를 사용하여 url image 추가

--- MagazineProject/TravelTableViewController.swift
+++ MagazineProject/TravelTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 struct Magazine {
     let title: String
@@ -36,13 +37,43 @@ class TravelTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "magazineCell", for: indexPath) as! TravelTableViewCell
         
         let data = list[indexPath.row]
+        let urlString = data.photo_image
+        let url = URL(string: urlString)
+        
+        let dateString = data.date
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "yyMMdd"
+        guard let finalDate = myFormatter.date(from: dateString) else {
+            cell.dateLabel.text = ""
+            return cell
+        }
+        
+        let dateOutterFormat = DateFormatter()
+        dateOutterFormat.dateFormat = "yy년 MM월 dd일"
+        let finalDateString = dateOutterFormat.string(from: finalDate)
+        cell.dateLabel.text = finalDateString
+                        
+        cell.MagazineImageView.kf.setImage(with: url)
+        cell.MagazineImageView.contentMode = .scaleToFill
+        cell.MagazineImageView.layer.cornerRadius = 15
         
         cell.titleLabel.text = data.title
+        cell.titleLabel.textColor = .darkGray
         cell.titleLabel.numberOfLines = 0
         cell.titleLabel.textAlignment = .left
         cell.titleLabel.font = .boldSystemFont(ofSize: 25)
         
+        cell.subtitleLabel.text = data.subtitle
+        cell.subtitleLabel.textColor = .lightGray
+        cell.subtitleLabel.numberOfLines = 0
+        cell.subtitleLabel.font = .boldSystemFont(ofSize: 17)
+        cell.subtitleLabel.textAlignment = .left
+        
+        cell.dateLabel.text = data.date
+        cell.dateLabel.textAlignment = .right
+        
         return cell
     }
-
+    
+    
 }
